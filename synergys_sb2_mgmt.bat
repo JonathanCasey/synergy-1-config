db11a1bf361227ae28973fdaa700c6a2ca7579d6
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-02-27 18:50:25 -0500

    Added office IR script.  Filled in most of SB2 conf file.  Added client call to server scripts since they may be clients if booted 2nd.

--- synergys_sb2_mgmt.bat
+++ synergys_sb2_mgmt.bat
@@ -2,6 +2,9 @@
 
 :: Be sure to add `C:\Program Files\Synergy` (or equivalent) to system Path.
 
+:: 0. Try connecting to P52S if it is already up
+synergyc_mgmt.bat JC-SB2
+
 :: 1. Try hosting on JC-SB2 wired
 synergys --no-restart --address 192.168.1.52 --name JC-SB2-wired --config synergy_office_sb2_right.conf
 
