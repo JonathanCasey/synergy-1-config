066b1bc532cc71c67840fce1beb435aba4349605
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-02-28 09:04:53 -0500

    Cleaned up config files, p52s config updated to match sb2 as appropriate (untested).  Re-enabled server trying to be client first.

--- synergys_p52s_mgmt.bat
+++ synergys_p52s_mgmt.bat
@@ -3,7 +3,7 @@
 :: Be sure to add `C:\Program Files\Synergy` (or equivalent) to system Path.
 
 :: 0. Try connecting to SB2 if it is already up
-synergyc_mgmt.bat VS-P52S
+call synergyc_mgmt.bat VS-P52S
 
 :: 1. Try hosting on VS-P52S wired
 synergys --no-restart --address 192.168.1.54 --name VS-P52S-wired --config synergy_office_p52s_right.conf
