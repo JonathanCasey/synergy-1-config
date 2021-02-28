1e2df5f80cbb8d6fbce518bb246d1ba7cf03094b
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-02-27 19:35:30 -0500

    Fixed client script trying to connect to same address in all 4 attempts.

--- synergyc_mgmt.bat
+++ synergyc_mgmt.bat
@@ -6,10 +6,10 @@
 synergyc --no-restart --name %1 192.168.1.52
 
 :: 2. Try connecting to JC-SB2 wireless
-synergyc --no-restart --name %1 192.168.1.52
+synergyc --no-restart --name %1 192.168.1.51
 
 :: 3. Try connecting to VS-P52S wired
-synergyc --no-restart --name %1 192.168.1.52
+synergyc --no-restart --name %1 192.168.1.54
 
 :: 4. Try connecting to VS-P52S wireless
-synergyc --no-restart --name %1 192.168.1.52
+synergyc --no-restart --name %1 192.168.1.53
